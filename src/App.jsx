5643efbefbdd45541853b173dd54a78bdb1c6dac
Jose Maria Ureña Torres <chematorres@Joses-MacBook-Air.local>
2023-01-03 19:36:21 -0600

    making the map of the api

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,37 @@
 import React from 'react'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 
 const App = () => {
+
+  const [characters, setCharacters] = useState([])
 
 useEffect(()=>{
   async function fetchData(){
     const response = await fetch("https://rickandmortyapi.com/api/character")
     const data = await response.json()
-    console.log(data);
+   setCharacters(data.results);
+   console.log(characters);
   }
-
   fetchData()
 },[])
 
+
+
   return (
-    <h1>Hello World</h1>
+    <div>
+      <h1>Rick and Morty</h1>
+      {
+        characters.map(({id,image,gender,name}) =>{
+          return(
+          <div key={id}>
+            <h2>{name}</h2>
+            <img src={image} alt={name} />
+          </div>
+          )
+        })
+      }
+    </div>
   )
 }
 
