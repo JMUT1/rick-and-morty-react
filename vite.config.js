943b525a279ae63027c199242e1a5a6c3f35146d
Jose Maria Ureña Torres <chematorres@Joses-MacBook-Air.local>
2023-01-04 11:09:31 -0600

    working on the deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://JMUT1.github.io/rick-and-morty-react",
+  base: "https://jmut1.github.io/rick-and-morty-react/",
   plugins: [react()],
 })
