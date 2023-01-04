f41a0f97671f586f42034206c8be96a342058b1e
Jose Maria Ureña Torres <chematorres@Joses-MacBook-Air.local>
2023-01-04 10:30:29 -0600

    making the pagination of the pages using the useState

--- src/components/CharacterList.jsx
+++ src/components/CharacterList.jsx
@@ -2,14 +2,23 @@ import { useEffect, useState } from 'react'
 import Character from "./Character"
 
 
-const CharacterList = () => {
+const NavPage = ({page, setPage})=>{
+    return (
+        <header className= "d-flex justify-content-between align-items-center">
+            <p>Page:{page} </p>
+            <button className='btn btn-primary' onClick={()=> setPage(2)}>Page 2</button>
+        </header>
+    )
+}
 
+const CharacterList = () => {
     const [characters, setCharacters] = useState([])
     const [loading, setLoading] = useState(true);
+    const [page, setPage] = useState(1)
 
     useEffect(()=>{
       async function fetchData(){
-        const response = await fetch("https://rickandmortyapi.com/api/character")
+        const response = await fetch(`https://rickandmortyapi.com/api/character?page=${page}`)
         const data = await response.json()
        setCharacters(data.results);
        setTimeout(()=>{
@@ -18,10 +27,11 @@ const CharacterList = () => {
        console.log(characters);
       }
       fetchData()
-    },[])
+    },[page])
     
   return (
     <div className='container'>
+        <NavPage page={page} setPage={setPage}/>
         {
             loading ? (<h1 className='d-flex justify-content-center '>Loading...</h1> ): (
                 <div className='row'>
