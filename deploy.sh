8d2acb363f7cf3a9e19be4c08226bbabe4d15246
Jose Maria Ureña Torres <chematorres@Joses-MacBook-Air.local>
2023-01-05 10:06:05 -0600

    changes to the deplo.sh

--- deploy.sh
+++ deploy.sh
@@ -21,9 +21,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+git push -f git@github.com:JMUT1/rick-and-morty-react.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:jmut1/rick-and-morty-react.git main:gh-pages
+git push -f git@github.com:JMUT1/rick-and-morty-react.git main:gh-pages
 
 cd -
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://jmut1.github.io/rick-and-morty-react/",
+  base: "https://JMUT1.github.io/rick-and-morty-react/",
   plugins: [react()],
 })
+
